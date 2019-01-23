2227da020ab82c47115617fb2cbdfd9b37198465
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-23 16:49:06 +0000

    Feature: A cell can identify if is will survive.

--- src/test/java/CellTests.java
+++ src/test/java/CellTests.java
@@ -7,6 +7,15 @@ import static junit.framework.TestCase.assertTrue;
 
 public class CellTests {
     @Test
+    public void a_cell_can_identify_if_it_will_survive() {
+        Cell cell = new Cell(new Coordinate(0,  0), CellState.Alive);
+        Cell cell1 = new Cell(new Coordinate(0, 1), CellState.Alive);
+        Cell cell2 = new Cell(new Coordinate(1, 0), CellState.Alive);
+        Population population = new Population(asList(cell.getCoordinate(), cell1.getCoordinate(), cell2.getCoordinate()));
+        assertTrue(cell.isSurvivor(population));
+    }
+
+    @Test
     public void a_cell_identifies_its_living_neighbors_from_the_population() {
         Cell cell = new Cell(new Coordinate(0,0), CellState.Alive);
         Cell livingNeighbor = new Cell(new Coordinate(0, 1), CellState.Alive);
