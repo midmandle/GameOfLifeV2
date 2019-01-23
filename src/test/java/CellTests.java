6a89d79884abca371c0b9ae2dbfde4a920c21dd4
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-23 17:01:56 +0000

    Feature: A cell can identify if it will be born.

--- src/test/java/CellTests.java
+++ src/test/java/CellTests.java
@@ -25,6 +25,17 @@ public class CellTests {
     }
 
     @Test
+    public void a_cell_can_identify_if_it_will_be_born() {
+        Cell cell = new Cell(new Coordinate(0,  0), CellState.Alive);
+        Cell cell1 = new Cell(new Coordinate(0, 1), CellState.Alive);
+        Cell cell2 = new Cell(new Coordinate(0, 2), CellState.Alive);
+        Cell cell3 = new Cell(new Coordinate(1, 1), CellState.Dead);
+        Population population = new Population(asList(cell.getCoordinate(), cell1.getCoordinate(), cell2.getCoordinate()));
+
+        assertTrue(cell3.isBirth(population));
+    }
+
+    @Test
     public void a_cell_identifies_its_living_neighbors_from_the_population() {
         Cell cell = new Cell(new Coordinate(0,0), CellState.Alive);
         Cell livingNeighbor = new Cell(new Coordinate(0, 1), CellState.Alive);
