e4f2db58305420380de6d5048813408614c7b7dd
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-24 14:38:38 +0000

    Feature: Added additional parameterized test to confirm correct execution of all the individual rules applied together.

--- src/test/java/TickTests.java
+++ src/test/java/TickTests.java
@@ -1,12 +1,17 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
+@RunWith(JUnitParamsRunner.class)
 public class TickTests {
     @Test
     public void a_world_with_no_living_cells_returns_a_world_without_living_cells() {
@@ -80,5 +85,62 @@ public class TickTests {
                 new Coordinate(1, 1)
         );
         assertTrue(nextLivingLocations.containsAll(expectedNextLivingLocations));
+    }
+
+
+    @Test
+    @Parameters(method="transform1, transform2")
+    public void a_world_produces_other_worlds(List<Coordinate> initialLocations, List<Coordinate> expectedNextLocations) {
+        World world = new World(initialLocations);
+        World nextWorld = world.tick();
+        List<Coordinate> nextLivingLocations = nextWorld.getLivingCellLocations();
+        assertTrue(nextLivingLocations.containsAll(expectedNextLocations));
+    }
+
+    public static Object[] transform1() {
+        return new Object[] {
+                Arrays.asList(
+                        new Coordinate(0,0),
+                        new Coordinate(1, 1),
+                        new Coordinate(2, 0)
+                ),
+                Arrays.asList(
+                        new Coordinate(1, 0),
+                        new Coordinate(1, 1)
+                )
+        };
+    }
+
+    public static Object[] transform2() {
+        return new Object[] {
+                Arrays.asList(
+                        new Coordinate(0,0),
+                        new Coordinate(1, 0),
+                        new Coordinate(2, 0)
+                ),
+                Arrays.asList(
+                        new Coordinate(1, 0),
+                        new Coordinate(1, 1),
+                        new Coordinate(1, -1)
+                )
+        };
+    }
+
+    public static Object[] transform3() {
+        return new Object[] {
+                Arrays.asList(
+                        new Coordinate(0,0),
+                        new Coordinate(2,0),
+                        new Coordinate(1, 1),
+                        new Coordinate(0, 2),
+                        new Coordinate(2, 2)
+                ),
+                Arrays.asList(
+                        new Coordinate(1, 0),
+                        new Coordinate(0, 1),
+                        new Coordinate(2, 1),
+                        new Coordinate(1, 2)
+                )
+        };
     }
 }
